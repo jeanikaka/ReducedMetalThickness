4025b1841b8fd7bfbc8f0ee84334586ff6c100e9
Jean <kasymovjean@gmail.com>
2022-08-01 01:06:15 +0300

    Filtered collector created

--- ReducedMetalThickness/Main.cs
+++ ReducedMetalThickness/Main.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,15 @@ using System.Threading.Tasks;
 
 namespace ReducedMetalThickness
 {
-
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
     public class Main : IExternalCommand{
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements){
             Document doc = commandData.Application.ActiveUIDocument.Document;
-            List<Element> element = new FilteredElementCollector(doc).WhereElementIsNotElementType().ToList();
+            Selection selection = commandData.Application.ActiveUIDocument.Selection;
+            List<ElementId> selElems = selection.PickObjects(ObjectType.Element, "Выберите элементы").Select(it=>doc.GetElement(it).Id).ToList();
+            List<Element> element = new FilteredElementCollector(doc, selElems)
+                .WhereElementIsNotElementType().ToList();
             return Result.Succeeded;
         }
     }
