4387e2c5f937b21a4a9109d7011fd7745e05a774
Jean <kasymovjean@gmail.com>
2022-07-31 19:01:27 +0300

    Initial commit

---
+++ ReducedMetalThickness/Main.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReducedMetalThickness
+{
+
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class Main : IExternalCommand{
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements){
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            List<Element> element = new FilteredElementCollector(doc).WhereElementIsNotElementType().ToList();
+            return Result.Succeeded;
+        }
+    }
+}
